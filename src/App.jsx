9827165a80cf1decc011b38353884d4ce78c29ef
Tunahan Karakaya <bstunahan@gmail.com>
2024-03-26 03:31:13 +0100

    added header file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-
+import React from 'react';
+import Header from './Header.jsx';
 
 function App() {
 
   return (
     <>
-     
+       <Header />
     </>
   )
 }
